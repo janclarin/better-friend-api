883f2faca09ea04b9f46d300f1f7ad4d9bfeeae9
Jonathan Wan <jnthnwn@gmail.com>
2017-02-21 20:55:37 -0700

    Testing the webhook endpoint

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
-
 app.get('/webhook', (req, res) => {
-  res.send('this is the webhook endpoint. try POSTing.');
+  res.send(200);
 });
 
 app.post('/webhook', (req, res) => {
   console.log(req.body);
   res.send(200);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello world test');
 });
 
 // Start the server.
