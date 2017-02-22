bb20edca03b369544d1b6f7970925a6a692c1c0d
Jan Clarin <hi@janclarin.com>
2017-02-21 20:22:06 -0700

    Add basic server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+// Start the server.
+app.listen(port, () => {
+  console.log("Running on localhost:" + port);
+});
