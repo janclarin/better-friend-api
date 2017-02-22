c013359553255388112aa0c7674166d1619e3665
Jan Clarin <hi@janclarin.com>
2017-02-22 09:25:50 -0700

    Only auto-comment for feed updates

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -26,28 +26,32 @@ router.post('/facebook', (req, res) => {
   let entry = req.body.entry[0];
   let userId = entry.id;
   let changedFields = entry.changed_fields;
-  let commentMessage = 'Very nice!';
 
-  console.log(changedFields);
-  return;
-
-  database.getAuthTokenForUser(userId, (err, accessToken) => {
-    console.log("Access token: " + accessToken);
-    graphApi.getLastFeedItemId(userId, accessToken, (err, feedItemId) => {
+  // Auto-comment if it was a feed update.
+  if (changedFields.indexOf('feed') !== -1) {
+    let commentMessage = 'Cool story!';
+    database.getAuthTokenForUser(userId, (err, accessToken) => {
       if (err) {
         console.log(err);
         return;
       }
-      console.log("Last feed item " + feedItemId);
-      graphApi.commentOnFeedItem(feedItemId, accessToken, commentMessage, (err, commentId) => {
+      console.log("Access token: " + accessToken);
+      graphApi.getLastFeedItemId(userId, accessToken, (err, feedItemId) => {
         if (err) {
           console.log(err);
           return;
         }
-        console.log("Auto-commented on feed item " + feedItemId);
+        console.log("Last feed item " + feedItemId);
+        graphApi.commentOnFeedItem(feedItemId, accessToken, commentMessage, (err, commentId) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log("Auto-commented on feed item " + feedItemId);
+        });
       });
     });
-  });
+  }
 
   // do stuff with the update
   lastReceivedUpdates.push(req.body);
