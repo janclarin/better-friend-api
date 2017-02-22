adef5b4cbd9c4d0bbb026a39b47f992f2182e175
Jarrett Spiker <jspiker@shaw.ca>
2017-02-22 10:46:46 -0700

    Ref heroku over localhost

--- auth/facebook.js
+++ auth/facebook.js
@@ -7,7 +7,7 @@ const database = require("../database.js");
 // Facebook app info.
 const facebookAppId = process.env.FB_APP_ID || '373335136385913';
 const facebookAppSecret = process.env.FB_APP_SECRET || 'c9cb9e46a67253fa8988d1cbd6fc04ce';
-const facebookCallbackUrl = process.env.FB_CALLBACK_URL || 'http://localhost:3000/auth/facebook/callback';
+const facebookCallbackUrl = process.env.FB_CALLBACK_URL || 'https://betterfriend.herokuapp.com/auth/facebook/callback';
 
 // Configure passport.
 passport.use(new FacebookStrategy({
