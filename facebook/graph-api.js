131596d5a82199367b47f6c13ef8f961d5280d68
Jan Clarin <hi@janclarin.com>
2017-02-22 04:14:08 -0700

    Add commentOnFeedItem function

--- facebook/graph-api.js
+++ facebook/graph-api.js
@@ -2,11 +2,27 @@ const fbgraph = require('fbgraph');
 fbgraph.setVersion('2.8');
 
 // Pass a function as callback, e.g. function(birthday).
-function getBirthday(fbUserId, fbUserAccessToken, callback) {
-  const requestUrl = fbUserId + '?fields=birthday&access_token=' + fbUserAccessToken;
+function getBirthday(userId, userAccessToken, callback) {
+  const requestUrl = userId + '?fields=birthday&access_token=' + userAccessToken;
   fbgraph.get(requestUrl, (err, res) => {
     callback(res.birthday);
-  })
+  });
+}
+
+function getLastFeedItemId(userId, userAccessToken, callback) {
+  fbgraph.get(userId + '/feed?limit=1&access_token=' + userAccessToken, (err, res) => {
+    callback(res.data[0].id);
+  });
+}
+
+function commentOnFeedItem(feedItemId, userAccessToken, comment, callback) {
+  const postUrl = feedItemId + '/comments?access_token=' + userAccessToken + '&message=' + comment;
+  fbgraph.post(postUrl, (err, res) => {
+    const newCommentId = res.id;
+    callback(newCommentId);
+  });
 }
 
 exports.getBirthday = getBirthday;
+exports.getLastFeedItemId = getLastFeedItemId;
+exports.commentOnFeedItem = commentOnFeedItem;
