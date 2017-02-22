4e27c12ed1f281ea76952d4a29fe05a5808a3a16
Jarrett Spiker <jspiker@shaw.ca>
2017-02-22 02:45:00 -0700

    Allow birthday settings querying

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -1,34 +1,41 @@
 const database = require("../database");
 
+let handleNoUserFound = function(err, users, res){
+  if(err){
+    return res.status(503).json({
+      "errors": [{
+        "status": "503",
+        "title": "Unknown Error",
+        "detail": "Unknown Error"
+      }]
+    });
+  }
+  if(users.length == 0){
+    return res.status(504).json({
+      "errors": [{
+        "status": "504",
+        "title": "User not found",
+        "detail": "User not found"
+      }]
+    });
+  }
+};
+
+
 exports.getUserInfo = function (req, res) {
   let user_id = req.params.user_id;
 
   database.findUser(user_id, (err, users) => {
 
-    if(err){
-      return res.status(503).json({
-        "errors": [{
-          "status": "503",
-          "title": "Unknown Error",
-          "detail": "Unknown Error"
-        }]
-      });
-    }
-    if(users.length == 0){
-      return res.status(504).json({
-        "errors": [{
-          "status": "504",
-          "title": "User not found",
-          "detail": "User not found"
-        }]
-      });
+    if(err || users.length == 0){
+      return handleNoUserFound(err, users, res);
     }
 
     let user = users[0];
     return res.status(200).json({"data": {
       "id": user.facebookUid,
-      "birthday" : user.birthday,
-      "name": user.name
+      "name": user.name,
+      "birthday": user.birthday
     }});
 
 
@@ -36,7 +43,27 @@ exports.getUserInfo = function (req, res) {
 };
 
 exports.getBirthdaySettings = function(req, res){
-  //TODO
+  let user_id = req.params.user_id;
+
+  database.findUser(user_id, (err, users) => {
+
+    if(err || users.length == 0){
+      return handleNoUserFound(err, users, res);
+    }
+
+    let user = users[0];
+    return res.status(200).json({"data": {
+      "id": user.facebookUid,
+      "name": user.name,
+      "birthdaySettings" : {
+        "isEnabled" : user.birthdaySettings.isEnabled,
+        "useEmoji" : user.birthdaySettings.useEmoji,
+        "callByName" : user.birthdaySettings.callByName
+      }
+    }});
+
+
+  });
 };
 
 exports.updateBirthdaySettings = function(req, res){
