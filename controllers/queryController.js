cd39e03d31935285abb38c7bee778d94c7b1e32f
Jarrett Spiker <jspiker@shaw.ca>
2017-02-22 03:18:29 -0700

    Allow updating birthday settings

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -67,7 +67,32 @@ exports.getBirthdaySettings = function(req, res){
 };
 
 exports.updateBirthdaySettings = function(req, res){
-  //TODO
+  let user_id = req.params.user_id;
+  database.findUser(user_id, (err, users) => {
+
+    if(err || users.length == 0){
+      return handleNoUserFound(err, users, res);
+    }
+
+    let user = users[0];
+    let birthdaySettings = user.birthdaySettings;
+    birthdaySettings.isEnabled = req.body.data.birthdaySettings.isEnabled;
+    birthdaySettings.callByName = req.body.data.birthdaySettings.callByName;
+    birthdaySettings.useEmoji = req.body.data.birthdaySettings.useEmoji;
+
+    return user.save((err, update) => {
+      if(err){
+        return res.status(500).json({
+          "errors": [{
+            "status": "500",
+            "title": "Update Failed",
+            "detail": "Update Failed"
+          }]
+        });
+      }
+      return res.status(200).json({"status": "success"});
+    });
+  });
 };
 
 
