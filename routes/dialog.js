b88d5d79a980d34080352bd645eb261a4fe06da8
Jan Clarin <hi@janclarin.com>
2017-02-22 04:00:28 -0700

    Fix calgaryhacks.com redirect link

--- routes/dialog.js
+++ routes/dialog.js
@@ -8,7 +8,7 @@ const facebookDialogCallbackUrl = process.env.FB_DIALOG_CALLBACK || 'http://loca
 const dialogRedirectUrl =
   'https://www.facebook.com/dialog/feed?app_id=' + facebookAppId +
   '&display=popup&amp;caption=An%20example%20caption' +
-  '&link=http%3A%2F%2Fcalgaryhacks.com2F' +
+  '&link=http%3A%2F%2Fcalgaryhacks.com' +
   '&from=100015522361747&to=100015418719862' +
   '&redirect_uri=' + facebookDialogCallbackUrl;
 
