5bd71316eaaca3859235187606bbbb1b886cafe7
Jan Clarin <hi@janclarin.com>
2017-02-22 02:26:49 -0700

    Add getBirthday graph-api function

--- facebook/graph-api.js
+++ facebook/graph-api.js
@@ -1,3 +1,12 @@
-const http = require('http');
+const fbgraph = require('fbgraph');
+fbgraph.setVersion('2.8');
 
+function getBirthday(fbUserId, fbUserAccessToken, callback) {
+  const requestUrl = fbUserId + '?fields=birthday&access_token=' + fbUserAccessToken;
+  fbgraph.get(requestUrl, (err, res) => {
+    console.log(res);
+    callback(res);
+  })
+}
 
+exports.getBirthday = getBirthday;
