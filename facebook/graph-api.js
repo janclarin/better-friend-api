3593e0788f3981fdf6f08d09181738b187b44023
Jan Clarin <hi@janclarin.com>
2017-02-22 02:29:10 -0700

    Only return birthday for getBirthday

--- facebook/graph-api.js
+++ facebook/graph-api.js
@@ -1,11 +1,11 @@
 const fbgraph = require('fbgraph');
 fbgraph.setVersion('2.8');
 
+// Pass a function as callback, e.g. function(birthday).
 function getBirthday(fbUserId, fbUserAccessToken, callback) {
   const requestUrl = fbUserId + '?fields=birthday&access_token=' + fbUserAccessToken;
   fbgraph.get(requestUrl, (err, res) => {
-    console.log(res);
-    callback(res);
+    callback(res.birthday);
   })
 }
 
